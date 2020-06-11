555800de9452835a8a1b854bf3881dc5ca0ce2ff
Hossein Zafari <ho3einzafari@gmail.com>
2020-06-11 13:23:38 +0430

    Add GLog Class Helper

---
+++ app/src/main/java/hosseinzafari/github/sales/util/GLog.java
@@ -0,0 +1,11 @@
+package hosseinzafari.github.sales.util;
+/*
+
+@created in 11/06/2020 - 01:10 PM
+@project Sales
+@author Hossein Zafari 
+@email  hosseinzafari2000@gmail.com 
+*/
+
+public class GLog {
+}
