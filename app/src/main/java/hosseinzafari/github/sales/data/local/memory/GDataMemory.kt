240078c2c1e4588b98aadda7a6badab649123d9e
Hossein Zafari <ho3einzafari@gmail.com>
2020-06-17 09:58:25 +0430

    Create Function CityData

---
+++ app/src/main/java/hosseinzafari/github/sales/data/local/memory/GDataMemory.kt
@@ -0,0 +1,65 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020.  Hossein Zafari/novinfar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package hosseinzafari.github.sales.data.local.memory
+
+/*
+@created in 17/06/2020 - 9:33 AM
+@project Sales
+@author Hossein Zafari 
+@email  hosseinzafari2000@gmail.com 
+*/
+
+fun getCityData() = listOf(
+       " آذربایجان شرقی"    ,
+       " آذربایجان غربی"        ,
+       " اردبیل"   ,
+       " اصفهان"            ,
+       "البرز"  ,
+       "ایلام"            ,
+       "بوشهر"            ,
+       "تهران"   ,
+       " چهارمحال و بختیاری"    ,
+       " خراسان جنوبی"          ,
+       " خراسان رضوی"   ,
+       " خراسان شمالی"   ,
+       "خوزستان" ,
+       "زنجان" ,
+       "سمنان" ,
+       " سیستان و بلوچستان"     ,
+       "فارس" ,
+       "قزوین" ,
+       "قم" ,
+       "کردستان" ,
+       "کرمان" ,
+       "کرمانشاه" ,
+       " کهگیلویه و بویراحمد"   ,
+       "گلستان" ,
+       "گیلان" ,
+       "لرستان" ,
+       "مازندران" ,
+       "مرکزی" ,
+       "هرمزگان" ,
+       "همدان" ,
+       "یزد"
+)
