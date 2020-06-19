0717018b40623d6e73bffdbcf5120e3a4d0d28c2
Hossein Zafari <ho3einzafari@gmail.com>
2020-06-19 18:23:14 +0430

    Add Adapter and Viewpager2 in StartActivity

--- app/src/main/java/hosseinzafari/github/sales/ui/activity/StartActivity.java
+++ app/src/main/java/hosseinzafari/github/sales/ui/activity/StartActivity.java
@@ -23,18 +23,46 @@
 
 package hosseinzafari.github.sales.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.viewpager2.widget.ViewPager2;
 
 import hosseinzafari.github.sales.R;
+import hosseinzafari.github.sales.adapter.SlideFragmentAdapter;
 import hosseinzafari.github.sales.core.GAppCompatActivity;
 
 public class StartActivity extends GAppCompatActivity {
+
+    private ViewPager2 viewpager;
+    private SlideFragmentAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        adapter = new SlideFragmentAdapter(this);
+        viewpager = findViewById(R.id.viewPager_Start);
+        prepareViewPager();
     }
+
+    private void prepareViewPager(){
+        viewpager.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        viewpager.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getCurrentPage() == 0){
+            super.onBackPressed();
+        } else {
+            viewpager.setCurrentItem(getCurrentPage() - 1);
+        }
+    }
+
+    private int getCurrentPage(){
+        return viewpager.getCurrentItem();
+    }
+
 }
