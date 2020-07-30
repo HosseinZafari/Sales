102f7182e2ac205a6877a1eb7f593f3c3d373ae9
Hossein <ho3einzafari@gmail.com>
2020-07-30 16:59:25 +0430

    Add Another Fake Data

--- app/src/main/java/hosseinzafari/github/sales/struct/DataProvider.java
+++ app/src/main/java/hosseinzafari/github/sales/struct/DataProvider.java
@@ -31,17 +31,17 @@ import hosseinzafari.github.sales.R;
 public class DataProvider {
 
 
-    public static List<Person> getdata(){
-        List<Person> people=new ArrayList();
+    public static List<Person> getdata() {
+        List<Person> people = new ArrayList();
 
-        Person person=new Person();
+        Person person = new Person();
         person.setName("امیرمهدی");
         person.setField("برنامه نویس");
         person.setDisc("متن کپی شده متن کپی شده متن کپی شده متن کپی شده متن کپی شده");
         person.setImage(R.drawable.face);
         people.add(person);
 
-        Person person2=new Person();
+        Person person2 = new Person();
         person2.setName("حسین ظفری");
         person2.setField("برنامه نویس");
         person2.setDisc("متن کپی شده متن کپی شده متن کپی شده متن کپی شده ");
@@ -49,7 +49,7 @@ public class DataProvider {
 
         people.add(person2);
 
-        Person person3=new Person();
+        Person person3 = new Person();
         person3.setName("علی مرادی");
         person3.setField("طراح");
         person3.setDisc("متن کپی شده متن کپی شده متن کپی شده متن کپی شده متن کپی شده متن کپی شده متن کپی شده متن کپی شده متن کپی شده");
@@ -57,7 +57,7 @@ public class DataProvider {
 
         people.add(person3);
 
-        Person person4=new Person();
+        Person person4 = new Person();
         person4.setName("مهدی مراد جم");
         person4.setField("طراح سایت");
         person4.setDisc("متن کپی شدهمتن کپی شدهمتن کپی شدهمتن کپی شدهمتن کپی شده");
@@ -65,7 +65,7 @@ public class DataProvider {
 
         people.add(person4);
 
-        Person person5=new Person();
+        Person person5 = new Person();
         person5.setName("محمد جهانگیری ");
         person5.setField("طراح سایت");
         person5.setDisc("متن کپی شدهمتن کپی شدهمتن کپی شدهمتن کپی شدهمتن کپی شده");
@@ -73,7 +73,7 @@ public class DataProvider {
 
         people.add(person5);
 
-        Person person6=new Person();
+        Person person6 = new Person();
         person6.setName("امیرعلی مهر جویی");
         person6.setField("بازاریاب");
         person6.setDisc("متن کپی شدهمتن کپی شدهمتن کپی شدهمتن کپی شدهمتن کپی شده");
@@ -81,16 +81,42 @@ public class DataProvider {
 
         people.add(person6);
 
-        Person person7=new Person();
+        Person person7 = new Person();
         person7.setName("مهدی مراد جم");
         person7.setField("طراح سایت");
         person7.setDisc("متن کپی شدهمتن کپی شدهمتن کپی شدهمتن کپی شدهمتن کپی شده");
         person7.setImage(R.drawable.face);
 
         people.add(person7);
+        people.add(person4);
+        people.add(person3);
+        people.add(person2);
+        people.add(person6);
+        people.add(person4);
+        people.add(person2);
+        people.add(person7);
+        people.add(person4);
+        people.add(person3);
+        people.add(person2);
+        people.add(person6);
+        people.add(person4);
+        people.add(person2);
+        people.add(person2);
+        people.add(person7);
+        people.add(person4);
+        people.add(person3);
+        people.add(person2);
+        people.add(person6);
+        people.add(person4);
+        people.add(person2);
+        people.add(person7);
+        people.add(person4);
+        people.add(person3);
+        people.add(person2);
+        people.add(person6);
+        people.add(person4);
+        people.add(person2);
 
-
-
-return people;
+        return people;
     }
 }
